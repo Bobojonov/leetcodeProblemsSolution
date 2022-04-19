cd69909692a0b02d35fc7413cec33eb8a1afdda6
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-04-19 18:29:47 +0500

    Time: 1005 ms (61.29%), Space: 86.1 MB (6.45%) - LeetHub

--- 929-unique-email-addresses/929-unique-email-addresses.scala
+++ 929-unique-email-addresses/929-unique-email-addresses.scala
@@ -1,13 +1,12 @@
 object Solution {
     def numUniqueEmails(emails: Array[String]): Int = {
-      val s = emails.foldLeft(List.empty[String]) { (str, e) =>
+    val s = emails.foldLeft(Set.empty[String]) { (str, e) =>
       val splitted = e.split("@")
-      println(splitted.toList)
       val filter = splitted.head
       val tail = splitted.last
       val s1 = filter.split('+').head
-      str :+ s1.replace(".", "") + "@" + tail
+      str + (s1.replace(".", "") + "@" + tail)
     }
-    s.toSet.size
-    }  
+    s.size
+  }  
 }
