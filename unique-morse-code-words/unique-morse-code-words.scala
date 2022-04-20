77838add1a8b7a851b9664e6282904bb474867e4
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-04-20 16:17:08 +0500

    Update unique-morse-code-words.scala

--- unique-morse-code-words/unique-morse-code-words.scala
+++ unique-morse-code-words/unique-morse-code-words.scala
@@ -1,14 +1,14 @@
 object Solution {
     def uniqueMorseRepresentations(words: Array[String]): Int = {
-    val mapMorse = (0 until 26).zip(Array(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
-      ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..")).toMap
-    val arr = "abcdefghijklmnopqrstuvwxyz".zipWithIndex.toMap
-    val result = words.foldLeft(List.empty[String]) { (list, e) =>
+      val mapMorse = (0 until 26).zip(Array(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+           ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..")).toMap
+      val arr = "abcdefghijklmnopqrstuvwxyz".zipWithIndex.toMap
+      val result = words.foldLeft(List.empty[String]) { (list, e) =>
       val x = e.foldLeft("") { (str, ch) =>
         str + mapMorse.get(arr.getOrElse(ch, -1)).mkString
+        }
+        list :+ x
       }
-      list :+ x
-    }
-    result.toSet.size
+      result.toSet.size
   }
 }
