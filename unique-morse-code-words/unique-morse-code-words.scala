5a4df5edece0fb089d68e9cd6becf548f881dd42
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-04-20 16:08:23 +0500

    Time: 767 ms, Memory: 75.2 MB - LeetHub

---
+++ unique-morse-code-words/unique-morse-code-words.scala
@@ -0,0 +1,14 @@
+object Solution {
+    def uniqueMorseRepresentations(words: Array[String]): Int = {
+    val mapMorse = (0 until 26).zip(Array(".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---",
+      ".--.", "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--..")).toMap
+    val arr = "abcdefghijklmnopqrstuvwxyz".zipWithIndex.toMap
+    val result = words.foldLeft(List.empty[String]) { (list, e) =>
+      val x = e.foldLeft("") { (str, ch) =>
+        str + mapMorse.get(arr.getOrElse(ch, -1)).mkString
+      }
+      list :+ x
+    }
+    result.toSet.size
+  }
+}
