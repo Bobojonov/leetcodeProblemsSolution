628fc977dacd852d040c9cf80548772f09b5eb4b
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-04-22 12:10:45 +0500

    Time: 691 ms (92.31%), Space: 54.1 MB (100.00%) - LeetHub

---
+++ 811-subdomain-visit-count/811-subdomain-visit-count.scala
@@ -0,0 +1,19 @@
+object Solution {
+    def subdomainVisits(cpdomains: Array[String]): List[String] = {
+    val count_pairs = cpdomains.foldLeft(Map.empty[String, Int]) { (acc, e) =>
+      val domain = e.split(" ")
+      val splitDots = domain(1).split('.')
+      val all_domains = splitDots.foldRight(List.empty[String], "") { (str, acc) =>
+        val s = str + acc._2
+        (s +: acc._1, '.' + s)
+      }
+      val visits = domain(0).toInt
+      all_domains._1.foldLeft(acc) { (map, e) =>
+        map + (e -> (map.getOrElse(e, 0) + visits))
+      }
+    }
+    count_pairs.foldLeft(List.empty[String]) { case (acc, (key, value)) =>
+      acc :+ (value.toString + " " + key)
+    }
+  }
+}
