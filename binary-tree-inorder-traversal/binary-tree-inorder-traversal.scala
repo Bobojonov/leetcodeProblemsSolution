5e35ef26a9aa0f2c7d9b42940289bf0f9d0a6686
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-05-17 12:02:54 +0500

    Time: 942 ms, Memory: 68.4 MB - LeetHub

---
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.scala
@@ -0,0 +1,14 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode(_value: Int = 0, _left: TreeNode = null, _right: TreeNode = null) {
+ *   var value: Int = _value
+ *   var left: TreeNode = _left
+ *   var right: TreeNode = _right
+ * }
+ */
+object Solution {
+    def inorderTraversal(root: TreeNode): List[Int] = {
+        if (root == null) List()
+        else inorderTraversal(root.left) ++ List(root.value) ++ inorderTraversal(root.right) 
+    }
+}
