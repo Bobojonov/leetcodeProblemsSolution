9a6ba56e2f2cc0c992a2bce28ed351b762dcaebc
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-07-06 14:54:47 +0500

    Time: 2 ms (26.71%), Space: 41.5 MB (75.66%) - LeetHub

---
+++ 266-palindrome-permutation/266-palindrome-permutation.java
@@ -0,0 +1,12 @@
+import java.util.HashSet;
+import java.util.Set;
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        Set<Character> set = new HashSet< >();
+        for (int i = 0; i < s.length(); i++){
+            if (!set.add(s.charAt(i)))
+                set.remove(s.charAt(i));
+        }
+        return set.size() <= 1;
+    }
+}
