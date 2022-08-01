d0993fd9771b91633a7871a5187db92824dc0c66
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-08-01 15:57:11 +0500

    Time: 3 ms (57.51%), Space: 48 MB (67.76%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] res = new int[n + 1];
+        res[0] = 0;
+        for (int i = 1; i <= n; i++){
+            if ((i & 1) == 0) res[i] = res[i >> 1];
+            else res[i] = res[i - 1] + 1;
+        }
+        return res;
+    }
+}
