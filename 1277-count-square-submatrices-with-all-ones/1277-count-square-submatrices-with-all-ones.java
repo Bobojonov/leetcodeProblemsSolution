66546d1558f64ac78202dfef8bcaa586af7c0d39
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-10-18 12:01:10 +0500

    Time: 6 ms (93.11%), Space: 70.6 MB (32.15%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.java
@@ -2,23 +2,24 @@ class Solution {
     public int countSquares(int[][] matrix) {
         int r = matrix.length;
         int c = matrix[0].length;
-        int[][] dp = new int[r][c];
+        int[][] squares = new int[r][c];
         int count = 0;
-        for (int i = 0; i < c; i++) {
-            dp[0][i] = matrix[0][i];
-            count += dp[0][i];
+        // count size == 1 
+        for (int i = 0; i < r; i++){
+            squares[i][0] = matrix[i][0];
+            count += squares[i][0];
         }
-        
-        for (int i = 1; i < r; i++) {
-            dp[i][0] = matrix[i][0];
-            count += dp[i][0];
+        for (int i = 1; i < c; i++){
+            squares[0][i] = matrix[0][i];
+            count += squares[0][i];
         }
-        
         for (int i = 1; i < r; i++) {
             for (int j = 1; j < c; j++) {
-                if (matrix[i][j] == 1)
-                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1]));
-                count += dp[i][j];
+                if (matrix[i][j] == 1) {
+                                        squares[i][j] = 1 + Math.min(Math.min(squares[i - 1][j - 1], squares[i][j - 1]), squares[i - 1][j]);
+
+                    count += squares[i][j];
+                }
             }
         }
         return count;
