79b18b1c7b393a66146cc08c6c1704ef390831e2
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-10-18 15:48:46 +0500

    Time: 7 ms (62.50%), Space: 41.4 MB (86.39%) - LeetHub

--- 0038-count-and-say/0038-count-and-say.java
+++ 0038-count-and-say/0038-count-and-say.java
@@ -3,8 +3,8 @@ class Solution {
         StringBuilder str = new StringBuilder("1");
         StringBuilder sb = new StringBuilder("1");
         for (int i = 2; i <= n; i++) {
-            str = new StringBuilder();
             int count = 1;
+            str = new StringBuilder();
             for (int j = 1; j < sb.length(); j++) {
                 if (sb.charAt(j) == sb.charAt(j - 1)) count++;
                 else {
@@ -16,7 +16,6 @@ class Solution {
             str.append(String.valueOf(count));
             str.append(sb.charAt(sb.length() - 1));
             sb = str;
-            
         }
         return new String(str);
     }
