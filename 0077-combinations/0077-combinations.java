51800b3ab161972d80c932cd16279a95317877ea
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-10-19 16:21:44 +0500

    Time: 17 ms (82.44%), Space: 45.3 MB (84.94%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,5 +1,5 @@
 class Solution {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> result = new LinkedList();
     public List<List<Integer>> combine(int n, int k) {
         backtrack(1, n, k, new LinkedList<Integer>());
         return result;
@@ -7,7 +7,7 @@ class Solution {
     
     public void backtrack(int start, int n, int k, LinkedList<Integer> combination) {
         if (combination.size() == k)
-            result.add(new ArrayList<>(combination));
+            result.add(new LinkedList<>(combination));
         for (int i = start; i <= n; i++) {
             combination.add(i);
             backtrack(i + 1, n, k, combination);
