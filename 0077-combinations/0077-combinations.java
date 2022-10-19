b9df6d707cb240b6d75323969d7d63cbf85ea873
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-10-19 16:22:40 +0500

    Time: 19 ms (80.65%), Space: 44.9 MB (86.19%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,16 +1,19 @@
 class Solution {
         List<List<Integer>> result = new LinkedList();
+        int n, k;
     public List<List<Integer>> combine(int n, int k) {
-        backtrack(1, n, k, new LinkedList<Integer>());
+        this.n = n;
+        this.k = k;
+        backtrack(1, new LinkedList<Integer>());
         return result;
     }
     
-    public void backtrack(int start, int n, int k, LinkedList<Integer> combination) {
+    public void backtrack(int start, LinkedList<Integer> combination) {
         if (combination.size() == k)
             result.add(new LinkedList<>(combination));
         for (int i = start; i <= n; i++) {
             combination.add(i);
-            backtrack(i + 1, n, k, combination);
+            backtrack(i + 1, combination);
             combination.removeLast();
         }
     }
