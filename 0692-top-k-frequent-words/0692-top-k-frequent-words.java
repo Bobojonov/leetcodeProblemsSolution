4bda43167f2a3117e22a881511bf28b48ea5c7ba
Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2022-10-19 09:30:21 +0500

    Time: 12 ms (58.40%), Space: 46 MB (5.39%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -0,0 +1,34 @@
+class Solution {
+    
+    public class Node {
+        String key;
+        int value;
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+    
+    public List<String> topKFrequent(String[] words, int k) {
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        for (String word: words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+            set.add(word);
+        }
+        
+        PriorityQueue<Node> sorted = new PriorityQueue<>((a, b) -> a.value == b.value ? a.key.compareTo(b.key): b.value - a.value);
+        Iterator<String> it = set.iterator();
+        while (it.hasNext()) {
+            String key = it.next();
+            Node node = new Node(key, map.get(key));
+            sorted.add(node);
+        }
+        List<String> result = new ArrayList<>();
+        while (!sorted.isEmpty() && k --> 0) {
+            Node node = sorted.poll();
+            result.add(node.key);
+        }
+        return result;
+    }
+}
