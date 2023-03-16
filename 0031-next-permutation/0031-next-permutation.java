de10ddef7438ca4835332011202ac32473fb40fe
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2023-03-16 15:14:26 +0500

    Time: 0 ms (100.00%), Space: 43 MB (21.48%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,22 +1,33 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        if (nums == null || nums.length <= 1) return;
-        int i = nums.length - 2;
-        while (i >= 0 && nums[i + 1] <= nums[i]) i--;
-        if (i >= 0) {
-            int j = nums.length - 1;
-            while (nums[i] >= nums[j]) j--;
-            swap(nums, i, j);
+        int pivot = indexOfLastPeak(nums) - 1;
+        if (pivot != -1) {
+            int nextPrefix = lastIndexOfGreater(nums, nums[pivot]);
+            swap(nums, pivot, nextPrefix);
         }
-        reverse(nums, i + 1, nums.length - 1);
+        reverseSuffix(nums, pivot + 1);
+    }
+
+    public int indexOfLastPeak(int[] nums) {
+        for (int i = nums.length - 1; i > 0; i--)
+            if (nums[i - 1] < nums[i]) return i;
+        return 0;
+    }
+    public int lastIndexOfGreater(int[] nums, int threshold) {
+        for (int i = nums.length - 1; i >= 0; --i) {
+            if (threshold < nums[i]) return i;
+        }
+        return -1;
     }
     public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-
-    public void reverse(int[] nums, int i, int j){
-        while (i < j) swap(nums, i++, j--);
+    public void reverseSuffix(int[] nums, int start) {
+        int end = nums.length - 1;
+        while (start < end) {
+            swap(nums, start++, end--);
+        }
     }
 }
