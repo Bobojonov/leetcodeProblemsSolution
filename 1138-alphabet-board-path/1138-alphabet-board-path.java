abeab7e4bbb1d50af9ba4646488af689ae278d7d
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2023-04-11 17:22:01 +0500

    Time: 0 ms (100.00%), Space: 40.8 MB (45.74%) - LeetHub

--- 1138-alphabet-board-path/1138-alphabet-board-path.java
+++ 1138-alphabet-board-path/1138-alphabet-board-path.java
@@ -3,12 +3,11 @@ class Solution {
         int x = 0, y = 0;
         StringBuilder sb = new StringBuilder();
         for (char c: target.toCharArray()) {
-            int x1 = (c - 'a') / 5;
-            int y1 = (c - 'a') % 5;
-            while (x1 < x) { x--; sb.append('U'); }
-            while (y1 > y) { y++; sb.append('R'); }
-            while (y1 < y) { y--; sb.append('L'); }
-            while (x1 > x) { x++; sb.append('D'); }
+            int x1 = (c - 'a') % 5, y1 = (c - 'a') / 5;
+            while (y1 < y) { y--; sb.append('U'); }
+            while (x1 > x) { x++; sb.append('R'); }
+            while (x1 < x) { x--; sb.append('L'); }
+            while (y1 > y) { y++; sb.append('D'); }
             sb.append('!');
         }
         return sb.toString();
