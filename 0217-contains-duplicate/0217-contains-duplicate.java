c0bbe21f2b217eb1f5c4f5636177321dafffa712
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2023-06-03 15:29:52 +0500

    Time: 20 ms (35.47%), Space: 57.1 MB (30.86%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,10 +1,8 @@
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int n : nums) {
-            map.put(n, map.getOrDefault(n, 0) + 1);
-            if (map.get(n) == 2) return true;
-        }
-        return false;
+         HashSet<Integer> set = new HashSet<>();
+        for (int n: nums)
+            set.add(n);
+        return nums.length != set.size();
     }
 }
