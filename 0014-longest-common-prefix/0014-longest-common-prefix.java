1fe25da2579fd327cd5e06c0deb0209ede328831
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-09 16:10:57 +0500

    Time: 0 ms (100%), Space: 41.6 MB (36.27%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -4,9 +4,7 @@ class Solution {
         for (int i = 1; i < strs.length; i++) {
             while (strs[i].indexOf(prefix) != 0) {
                 prefix = prefix.substring(0, prefix.length() - 1);
-                if (prefix.isEmpty()) {
-                    return "";
-                }
+                if (prefix.isEmpty()) return "";
             }
         }
         return prefix;
