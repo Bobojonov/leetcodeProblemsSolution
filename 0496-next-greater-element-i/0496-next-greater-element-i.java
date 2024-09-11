2420a2625d21ea7815982db9e8e3df4f7b3fea2a
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-11 16:03:30 +0500

    Time: 2 ms (99.06%), Space: 43.3 MB (72.39%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums2.length; i++)
-            map.put(nums2[i], i);
+        int[] res = new int[nums1.length];
+        int cnt = 0;
+        
         for (int i = 0; i < nums1.length; i++) {
-            int el = nums1[i];
-            nums1[i] = -1;
-            for (int j = map.get(el); j < nums2.length; j++) {
-                if (el < nums2[j]) {
-                    nums1[i] = nums2[j];
-                    break;
+            res[cnt++] = counter(nums1[i], nums2);
+        }
+        return res;
+    }
+    
+    public int counter(int i, int[] nums2) {
+        for (int j = 0; j < nums2.length; j++) {
+            if (nums2[j] == i) {
+                for (int k = j + 1; k < nums2.length; k++) {
+                    if (nums2[k] > i)
+                        return nums2[k];
                 }
             }
         }
-        return nums1;
+        return -1;
     }
 }
