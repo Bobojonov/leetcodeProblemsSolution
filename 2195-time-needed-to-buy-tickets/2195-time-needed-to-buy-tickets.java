a9ce6058cb970a1122bbd788e72da9dfa5066e3d
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-12 13:09:11 +0500

    Time: 0 ms (100%), Space: 40.9 MB (79.54%) - LeetHub

---
+++ 2195-time-needed-to-buy-tickets/2195-time-needed-to-buy-tickets.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int timeRequiredToBuy(int[] tickets, int k) {
+        int res = 0;
+        for (int i = 0; i < tickets.length; i++) {
+            if (i <= k) {
+                res += Math.min(tickets[i], tickets[k]);
+            } else 
+                res += Math.min(tickets[i], tickets[k] - 1);
+        }
+        return res;
+    }
+}
