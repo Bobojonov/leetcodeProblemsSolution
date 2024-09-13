98be4d256b392b6ff867ee3788a567d15372fcdd
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-13 17:48:52 +0500

    Time: 14 ms (61.86%), Space: 45.3 MB (75.23%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -1,17 +1,33 @@
 class Solution {
+    static class SortCharacters implements Comparable<SortCharacters> {
+        Character c;
+        Integer freq;
+        public SortCharacters(Character c, Integer freq) {
+            this.c = c;
+            this.freq = freq;
+        }
+
+        @Override
+        public int compareTo(SortCharacters o) {
+            return o.freq.compareTo(this.freq);
+        }
+    }
     public String frequencySort(String s) {
         Map<Character, Integer> map = new HashMap<>();
-        for (char c: s.toCharArray()) {
-            if (Character.isLowerCase(c))
-                map.put(c, map.getOrDefault(c, 0) + 1);
-            else map.put(c, map.getOrDefault(c, 0) + 1);
+        for (char c: s.toCharArray())
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        List<SortCharacters> list = new ArrayList<>();
+        for (Map.Entry<Character, Integer> entry: map.entrySet()) {
+            list.add(new SortCharacters(entry.getKey(), entry.getValue()));
         }
-        List<Character> characters = new ArrayList<>(map.keySet());
-        Collections.sort(characters, (a, b) -> map.get(b) - map.get(a));
-        StringBuilder sb = new StringBuilder();
-        for (char c: characters) {
-            sb.append(String.valueOf(c).repeat(map.get(c)));
+        Collections.sort(list);
+       StringBuilder result = new StringBuilder();
+        for (SortCharacters sc : list) {
+            for (int i = 0; i < sc.freq; i++) {
+                result.append(sc.c);
+            }
         }
-        return sb.toString();
+
+        return result.toString();
     }
 }
