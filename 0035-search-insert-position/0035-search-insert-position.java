7add413cbad80c3439b7d91d3c13c17822b61e0b
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-18 11:44:50 +0500

    Time: 0 ms (100%), Space: 43.4 MB (6.8%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l = 0, r = nums.length - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l;
+    }
+}
