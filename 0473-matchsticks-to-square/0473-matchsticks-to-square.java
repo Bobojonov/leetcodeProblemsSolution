286a173c51ce735a5a322028f2c0c339588e3b48
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-20 18:07:38 +0500

    Time: 3 ms (96.95%), Space: 41.1 MB (80.92%) - LeetHub

--- 0473-matchsticks-to-square/0473-matchsticks-to-square.java
+++ 0473-matchsticks-to-square/0473-matchsticks-to-square.java
@@ -1,33 +1,31 @@
 class Solution {
     public boolean makesquare(int[] matchsticks) {
         int sum = 0;
-        for (int num: matchsticks)
-            sum += num;
+        for (int sticks: matchsticks) sum += sticks;
         if (sum % 4 != 0 || sum == 0) return false;
         Arrays.sort(matchsticks);
         reverse(matchsticks);
         int side = sum / 4;
         int[] sides = {0, 0, 0, 0};
-        return dfs(matchsticks, sides, 0, side);
-    }
-    
-    public boolean dfs(int[] nums, int[] sides, int idx, int side) {
-        if (idx == nums.length) return true;
-        int curr = nums[idx];
-        for (int i = 0; i < 4; i++) {
-            if (i > 0 && sides[i] == sides[i - 1])
-                continue;
-            if (sides[i] + curr <= side) {
-                sides[i] += curr;
-                if (dfs(nums, sides, idx + 1, side))
-                    return true;
-                sides[i] -= curr;
-            }
+        return backtrack(0, side, sides, matchsticks);
         
+    }
+
+    public boolean backtrack(int start, int side, int[] sides, int[] matchsticks) {
+        if (start == matchsticks.length) return true;
+        int stick = matchsticks[start];
+        for (int i = 0; i < 4; i++) {
+            if (i > 0 && sides[i] == sides[i - 1]) continue;
+            if (sides[i] + stick <= side) {
+                sides[i] += stick;
+                if (backtrack(start + 1, side, sides, matchsticks)) return true;
+                sides[i] -= stick;
+            }
         }
         return false;
     }
-     public void reverse(int[] nums) {
+    
+    public void reverse(int[] nums) {
         int i = 0, j = nums.length - 1;
         while (i < j) {
             int temp = nums[i];
