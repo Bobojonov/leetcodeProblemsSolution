ca8c0bdbe1a93b5c9bd25e49e3c8e5db6e2fe54d
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-20 17:55:25 +0500

    Time: 0 ms (100%), Space: 42 MB (56.7%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,32 +1,30 @@
 class Solution {
-    public List<String> letterCombinations(String digits) {
+   public List<String> letterCombinations(String digits) {
         List<String> res = new ArrayList<>();
         if (digits.isEmpty()) return res;
-        Map<Integer, List<Character>> map = new HashMap<>();
-        map.put(2, List.of('a', 'b', 'c'));
-        map.put(3, List.of('d', 'e', 'f'));
-        map.put(4, List.of('g', 'h', 'i'));
-        map.put(5, List.of('j', 'k', 'l'));
-        map.put(6, List.of('m', 'n', 'o'));
-        map.put(7, List.of('p', 'q', 'r', 's'));
-        map.put(8, List.of('t', 'u', 'v'));
-        map.put(9, List.of('w', 'x', 'y', 'z'));
+        Map<Character, String> map = new HashMap<>();
+        map.put('2', "abc");
+        map.put('3', "def");
+        map.put('4', "ghi");
+        map.put('5', "jkl");
+        map.put('6', "mno");
+        map.put('7', "pqrs");
+        map.put('8', "tuv");
+        map.put('9', "wxyz");
         backtrack(0, new StringBuilder(), digits, res, map);
         return res;
     }
 
-    public void backtrack(int pos, StringBuilder sb, String digits, List<String> res, Map<Integer, List<Character>> map) {
+    public void backtrack(int pos, StringBuilder sb, String digits, List<String> res, Map<Character, String> map) {
         if (pos == digits.length()) {
-            if (sb.length() == digits.length()) res.add(sb.toString());
+            res.add(sb.toString());
             return;
         }
-        for (int i = pos; i < digits.length(); i++) {
-            List<Character> values = (map.get(digits.charAt(i) - '0'));
-            for (Character value : values) {
-                sb.append(value);
-                backtrack(i + 1, sb, digits, res, map);
-                sb.delete(sb.length() - 1, sb.length());
-            }
+        String values = map.get(digits.charAt(pos));
+        for (int i = 0; i < values.length(); i++) {
+            sb.append(values.charAt(i));
+            backtrack(pos + 1, sb, digits, res, map);
+            sb.deleteCharAt(sb.length() - 1);
         }
     }
 }
