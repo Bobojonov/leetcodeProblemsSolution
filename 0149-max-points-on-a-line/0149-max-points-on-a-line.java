24bd26d9a9b0eb16516e30e39d3bad89409f7fd7
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-24 14:22:04 +0500

    Time: 36 ms (36.7%), Space: 44.4 MB (65.22%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.java
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.java
@@ -1,24 +1,38 @@
 class Solution {
     public int maxPoints(int[][] points) {
-        int n = points.length;
-        if (n == 1) return 1;
+        if (points == null) return 0;
+        if (points.length <= 2) return points.length;
         int res = 1;
-        for (int i = 0; i < n; i++) {
-            Map<Double, Integer> map = new HashMap<>();
-            for (int j = i + 1; j < n; j++) {
-                double slopes = findSlopes(points[i], points[j]);
-                map.put(slopes, map.getOrDefault(slopes, 0) + 1);
-                res = Math.max(res, map.get(slopes));
+        for (int i = 0; i < points.length; i++) {
+            int[] point1 = points[i];
+            int duplicates = 0;
+            int max = 0;
+            Map<String, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < points.length; j++) {
+                int[] point2 = points[j];
+                int dx = point2[0] - point1[0];
+                int dy = point2[1] - point1[1];
+                if (dx == 0 && dy == 0) {
+                    duplicates++;
+                    continue;
+                }
+                int gcd = gcd(dx, dy);
+                dx /= gcd;
+                dy /= gcd;
+                if (dx < 0) {
+                    dx = -dx;
+                    dy = -dy;
+                }
+                String slope = dx + "/" + dy;
+                map.put(slope, map.getOrDefault(slope, 0) + 1);
+                max = Math.max(max, map.get(slope));
             }
-        
+            res = Math.max(res, max + duplicates + 1);
         }
-        return res + 1;
+        return res;
     }
-    public double findSlopes(int[] point1, int[] point2) {
-        int x1 = point1[0], x2 = point2[0];
-        int y1 = point1[1], y2 = point2[1];
-        if (x1 == x2) return Double.MAX_VALUE;
-        if (y1 == y2) return 0;
-        return (y2 - y1) * 1.0 / (x2 - x1);
-    } 
+
+    public static int gcd(int a, int b) {
+        return b == 0 ? a : gcd(b, a % b);
+    }
 }
