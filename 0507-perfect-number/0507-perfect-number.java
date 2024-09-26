e507699a7934f4911caa4f8169656d61b3cbedce
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-26 20:28:52 +0500

    Time: 2093 ms (37.75%), Space: 40.4 MB (42.21%) - LeetHub

---
+++ 0507-perfect-number/0507-perfect-number.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int sum = 0;
+        for (int i = 1; i <= num / 2; i++) {
+            if (num % i == 0) sum += i;
+        }
+        return sum == num;
+    }
+}
