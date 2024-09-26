edde065a62cedcae08966d2cec94815f5d36313a
Bobojon Bobojonov <49334271+Bobojonov@users.noreply.github.com>
2024-09-26 14:31:48 +0500

    Time: 3 ms (62.88%), Space: 42.2 MB (72.34%) - LeetHub

--- 0043-multiply-strings/0043-multiply-strings.java
+++ 0043-multiply-strings/0043-multiply-strings.java
@@ -1,32 +1,26 @@
 class Solution {
     public String multiply(String num1, String num2) {
-        if (num1.equals("0") || num2.equals("0")) return "0";
-        return multi(num1, num2);
-    }
-    
-    public String multi(String num1, String num2) {
-        int len = num1.length() + num2.length();
-        StringBuilder result = new StringBuilder();
-        for (int i = 0; i < len; i++)
-            result.append(0);
-        StringBuilder first = new StringBuilder(num1);
-        StringBuilder second = new StringBuilder(num2);
-        first.reverse();
-        second.reverse();
-        for (int i = 0; i < second.length(); i++) {
-            int digit2 = second.charAt(i) -'0';
-            for (int j = 0; j < first.length(); j++) {
-                int digit1 = first.charAt(j) -'0';
-                int pos = i + j;
-                int sum = result.charAt(pos) - '0';
-                int mult = digit1 * digit2 + sum;
-                result.setCharAt(pos, (char)(mult % 10 + '0'));
-                int value = (result.charAt(pos + 1) - '0') + mult / 10;
-                result.setCharAt(pos + 1, (char)(value + '0'));
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int[] res = new int[num1.length() + num2.length()];
+        num1 = new StringBuilder(num1).reverse().toString();
+        num2 = new StringBuilder(num2).reverse().toString();
+        for (int i = 0; i < num1.length(); i++) {
+            for (int j = 0; j < num2.length(); j++) {
+                int multiply = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                res[i + j] += multiply;
+                res[i + j + 1] += res[i + j] / 10;
+                res[i + j] %= 10;
             }
         }
-        if (result.charAt(result.length() - 1) == '0') result.deleteCharAt(result.length() - 1);
-        result.reverse();
-        return new String(result);
+        int idx = res.length - 1;
+        while (idx >= 0 && res[idx] == 0)
+            idx--;
+        StringBuilder sb = new StringBuilder();
+        for (int i = idx; i >= 0 ; i--) {
+            sb.append(res[i]);
+        }
+        return sb.toString();
     }
 }
